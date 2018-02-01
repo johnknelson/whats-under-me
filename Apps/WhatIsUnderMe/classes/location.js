3fbd3f2d62e45a372c480c4e68bb0d6813867cdc
johnknelson <johnknelson3@yahoo.com>
2018-01-31 22:47:15 -0600

    Plot antipode

--- Apps/WhatIsUnderMe/classes/location.js
+++ Apps/WhatIsUnderMe/classes/location.js
@@ -3,4 +3,27 @@ class Location {
         this.lat = lat
         this.lng = lng
     }
+
+    addLocationToMap(){
+        var pointEntity = viewer.entities.add({
+            name : "userAntipode",
+            position : Cesium.Cartesian3.fromDegrees(this.lng, this.lat),
+            point : {
+                pixelSize : 5,
+                color : Cesium.Color.RED,
+                outlineColor : Cesium.Color.WHITE,
+                outlineWidth : 2
+            },
+            label : {
+                text : "Your Antipode!!!",
+                font : '14pt monospace',
+                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+                outlineWidth : 2,
+                verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+                pixelOffset : new Cesium.Cartesian2(0, -9)
+            }
+        });
+
+        return pointEntity;
+    }
 }
